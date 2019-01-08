504967cb994783f178deb1965149e299b2bfb5da
ionic <ionicharnina@gmail.com>
2019-01-08 10:58:27 +0100

    nada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Camera } from '@ionic-native/camera';
+import { CameraMock } from '../../mocks/camera';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: Camera, useClass: CameraMock }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  // https://blog.ionicframework.com/ionic-native-mocks/
+  //No podemos crear nosotros todos los mocks. 
+  //Primero debemos ver si ya existen en algun sitio
+  // por ejemplo npm install @ionic-native-mocks/camera --save
+  //tambien tenemos que instalar el native real
+  //Los combinamos en el app.module
+  image:string=null;
+  constructor(private camera: Camera, private platform: Platform) {
+    
+   
   }
 
+  hacerFoto(){
+    this.platform.ready().then(() => {
+
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      }
+
+      this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        console.log("la imagen esta"+ imageData);
+        //y ahora a pintarla
+        this.image='data:image/jpeg;base64,$(imageData)';
+      }, (err) => {
+        // Handle error
+      });
+    });
+  }
 }
+
